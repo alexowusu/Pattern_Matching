8a9207255f11d0c640f195d68feb7c6797681be4
alexowusu <owusu3y@gmail.com>
2019-11-27 13:39:24 +0000

    pm

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -10,5 +10,19 @@ val books = List(new Book("Phoenix", "gaming",1990,"play"),
   )
 
   books.foreach(println)
+
+  def bookByTitle(books:List[Book]):Map[String, Book]= books.map(books=>books.title->books).toMap
+
+//  def ByTitle(books:List[Book]):Map[String, Book] = books.map(books=>books.title->books).toMap
+
+  val Title= List("gaming","anase","age of empire")
+  val bk = bookByTitle(books)
+  Title flatMap bk.get
+
+  for (t <- Title;
+       lb <- bk.get(t))
+    yield(t,lb)
+
+
 }
 
